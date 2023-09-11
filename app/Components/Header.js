9c0fb9b62ef83e7419b03f2fb7c8638396d28464
Lite W Kim <litek@orionhealth.com>
2023-09-12 09:06:06 +1200

    add tech stack page

--- app/Components/Header.js
+++ app/Components/Header.js
@@ -4,8 +4,13 @@ import Link from "next/link";
 const Header = () => {
   return (
     <div className="header">
-      <Link href="/">Lite KIM</Link>
-      <Link href="/Connect">Connect</Link>
+      <div className="header-left">
+        <Link href="/">Lite KIM</Link>
+        <Link href="/TechStack">TechStack</Link>
+      </div>
+      <div className="header-right">
+        <Link href="/Connect">Connect</Link>
+      </div>
     </div>
   )
 };
---
+++ app/TechStack/page.js
@@ -0,0 +1,130 @@
+"use client";
+
+import {
+  SiPython, SiJavascript, SiHtml5, SiCss3,
+  SiFlask, SiReact, SiNextdotjs,
+  SiMicrosoftsqlserver, SiOracle, SiPostgresql, SiSqlite,
+  SiDocker, SiKubernetes, SiAmazonaws,
+  SiAnsible, SiPuppet,
+  SiGit, SiGitlab, SiBitbucket, SiGithub,
+  SiVisualstudiocode, SiJira, SiConfluence, SiTrello,
+  SiRedis, SiNginx, SiWebpack
+} from "react-icons/si";
+
+const TechStack = props => {
+  return (
+    <div className="tech-stack-page">
+      <TechStackSection name="Languages">
+        <Labelled label="Python">
+          <SiPython />
+        </Labelled>
+        <Labelled label="Javascript">
+          <SiJavascript />
+        </Labelled>
+        <Labelled label="HTML">
+          <SiHtml5 />
+        </Labelled>
+        <Labelled label="CSS">
+          <SiCss3 />
+        </Labelled>
+      </TechStackSection>
+      <TechStackSection name="Frameworks">
+        <Labelled label="Flask">
+          <SiFlask />
+        </Labelled>
+        <Labelled label="React">
+          <SiReact />
+        </Labelled>
+        <Labelled label="Next.js">
+          <SiNextdotjs />
+        </Labelled>
+      </TechStackSection>
+      <TechStackSection name="Databases">
+        <Labelled label="SQL Server">
+          <SiMicrosoftsqlserver />
+        </Labelled>
+        <Labelled label="Oracle DB">
+          <SiOracle />
+        </Labelled>
+        <Labelled label="PostgreSQL">
+          <SiPostgresql />
+        </Labelled>
+        <Labelled label="SQLite">
+          <SiSqlite />
+        </Labelled>
+      </TechStackSection>
+      <TechStackSection name="Deployment">
+        <Labelled label="Docker">
+          <SiDocker />
+        </Labelled>
+        <Labelled label="Kubernetes">
+          <SiKubernetes />
+        </Labelled>
+        <Labelled label="AWS">
+          <SiAmazonaws />
+        </Labelled>
+      </TechStackSection>
+      <TechStackSection name="Automation">
+        <Labelled label="Ansible">
+          <SiAnsible />
+        </Labelled>
+      </TechStackSection>
+      <TechStackSection name="Version Control">
+        <Labelled label="Git CLI">
+          <SiGit />
+        </Labelled>
+        <Labelled label="Gitlab">
+          <SiGitlab />
+        </Labelled>
+        <Labelled label="Bitbucket">
+          <SiBitbucket />
+        </Labelled>
+        <Labelled label="Github">
+          <SiGithub />
+        </Labelled>
+      </TechStackSection>
+      <TechStackSection name="Software">
+        <Labelled label="VS Code">
+          <SiVisualstudiocode />
+        </Labelled>
+        <Labelled label="Jira">
+          <SiJira />
+        </Labelled>
+        <Labelled label="Confluence">
+          <SiConfluence />
+        </Labelled>
+        <Labelled label="Trello">
+          <SiTrello />
+        </Labelled>
+      </TechStackSection>
+    </div>
+  )
+};
+
+const TechStackSection = props => {
+  const { name, children } = props;
+
+  return (
+    <div className="tech-stack-section">
+      <div className="tech-stack-name">
+        {name}
+      </div>
+      <div className="tech-stack-list">
+        {children}
+      </div>
+    </div>
+  )
+};
+
+const Labelled = props => {
+  const { label, children } = props;
+  
+  return (
+    <div className="tech-stack-tech">
+      <div className="tech-icon">{children}</div>
+      <div className="tech-label">{label}</div>
+    </div>
+  )
+};
+
+export default TechStack;
